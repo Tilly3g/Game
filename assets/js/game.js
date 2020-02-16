d87f9fa2c7a075b27ecee6f4da4a693fbdfa5407
Tilly3g <tilly.gee@hotmail.co.uk>
2020-02-16 13:50:55 +0000

    fix bug with strict button and on/off switch only workig once

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,36 +9,40 @@ let level;
 let arraysMatch;
 let win;
 
+// constants
+const countDisplay = document.querySelector("#display-text");
+const onOff = document.querySelector("#on-off-switch");
+const strictButton = document.querySelector("#strict-button")
+
 // controls - game on/off
-$("#on-off-switch").addEventListener('click', (event) => {
-    if ($("#button-on").css("visibility", "hidden")) {
+onOff.addEventListener('click', (event) => {
+    if (on == false) {
         on = true;
         $("#button-on").css("visibility", "visible");
         $("#button-off").css("visibility", "hidden");
-        $("display-text").html("--");
-
-        } else if ($("#button-on").css("visibility", "visible")) {
-          on = false;
+        countDisplay.innerHTML = ("--");
+        } else if (on = true) {
+        on = false;
         $("#button-on").css("visibility", "hidden");
         $("#button-off").css("visibility", "visible");
-        $("display-text").html("");
+        countDisplay.innerHTML = ("");
         noColour();
         clearCount();
         }
 });
 
 // strict button
-$("#strict-button").addEventListener('click', (event) => {
-    if ($("#strict-button").css("background", "#e7ea09")) {
+strictButton.addEventListener('click', function() {
+    if (strict == false) {
         strict = true;
-        $("#strict-button").css("background", "#0d9e1f")
-    } else {
+        strictButton.style.background = '#0d9e1f';
+    } else if (strict = true) {
         strict = false;
-        $("#strict-button").css("background", "#e7ea09")
+        strictButton.style.background = '#e7ea09';
     }
 });
 
-// start button
+start button
 $("#start-button").addEventListener('click', (event) => {
     if (on || win) {
         run();
