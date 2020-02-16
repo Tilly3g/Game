57044661c9040a5835f7283584344c00ee9f2d01
Tilly3g <tilly.gee@hotmail.co.uk>
2020-02-16 15:43:00 +0000

    Add game functionality to generate random numbers and make these correspond to a colour lighting up

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,37 +1,44 @@
-// variables
+//variables
 let on = false;
 let strict = false;
-let play;
+let sound = true
+let playLights;
 let compOrder = [];
 let compTurn
 let playerOrder = [];
 let level;
-let arraysMatch;
+let playerCorrect;
 let win;
+let lightInterval;
 
-// constants
-const countDisplay = document.querySelector("#display-text");
-const onOff = document.querySelector("#on-off-switch");
-const strictButton = document.querySelector("#strict-button")
+//constants from html
+const countDisplay = document.getElementById("display-text");
+const onOff = document.getElementById("on-off-switch");
+const strictButton = document.getElementById("strict-button");
+const startButton = document.getElementById("start-button");
+const blueButton = document.getElementById("button-blue")
+const redButton = document.getElementById("button-red")
+const greenButton = document.getElementById("button-green")
+const yellowButton = document.getElementById("button-yellow")
 
-// controls - game on/off
+//controls - game on/off
 onOff.addEventListener('click', (event) => {
     if (on == false) {
         on = true;
         $("#button-on").css("visibility", "visible");
         $("#button-off").css("visibility", "hidden");
         countDisplay.innerHTML = ("--");
-        } else if (on = true) {
+    } else if (on = true) {
         on = false;
         $("#button-on").css("visibility", "hidden");
         $("#button-off").css("visibility", "visible");
         countDisplay.innerHTML = ("");
-        noColour();
-        clearCount();
-        }
+        //noLights();
+        clearInterval();
+    }
 });
 
-// strict button
+//strict button
 strictButton.addEventListener('click', function() {
     if (strict == false) {
         strict = true;
@@ -42,9 +49,62 @@ strictButton.addEventListener('click', function() {
     }
 });
 
-start button
-$("#start-button").addEventListener('click', (event) => {
-    if (on || win) {
+//start button
+startButton.addEventListener('click', function() {
+    if (on == true || win == true) {
         run();
     }
 })
+
+//Game play functionality
+function run() {
+    level = 1;
+    countDisplay.innerHTML = 1;
+    compOrder = [];
+    playerOrder = [];
+    playLights = 0;
+    playerCorrect = true;
+    for (var i = 0; i < 20; i++) {
+        compOrder.push(Math.floor(Math.random() * 4) + 1);
+    }
+    compTurn = true;
+    console.log(compOrder)
+    lightInterval = setInterval(lightUp, 1000)
+}
+
+function lightUp() {
+
+    if (compTurn == true) {
+        on = false;
+        //noLights();
+        setTimeout(function() {
+            if (compOrder[playLights] == 1) blue();
+            if (compOrder[playLights] == 2) red();
+            if (compOrder[playLights] == 3) green();
+            if (compOrder[playLights] == 4) yellow();
+            playLights++;
+        }, 500);
+    }
+
+    if (playLights == level) {
+        on = true;
+        compTurn = false;
+        //noLights();
+    }
+}
+
+function blue() {
+    blueButton.style.background = '#223cff';
+}
+
+function red() {
+    redButton.style.background = '#ff3131';
+}
+
+function green() {
+    greenButton.style.background = '#0bd224';
+}
+
+function yellow() {
+    yellowButton.style.background = '#fdff50';
+}
